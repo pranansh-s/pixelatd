8e2f9d105761ad832bee5d031869ae807f890289
Pranansh Singh <43909197+pranansh-s@users.noreply.github.com>
2022-06-03 13:43:00 +0530

    seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,7 +17,9 @@ import Document, {
         <Html>
           <Head>
           <link rel="icon" href="/favicon.ico" />
-          <meta name="description" content="Movie guessing game derived from the wordle fad"/>
+          <title>Pixelatd</title>
+          <meta name="description" content="Poster/Movie guessing game, where you guess movies from their blurred posters"/>
+          <meta property='og:title' content='Pixelatd'/>
           <link href="https://fonts.googleapis.com/css2?family=Francois+One&family=Gidugu&family=Hind+Madurai:wght@600&display=swap" rel="stylesheet" />
           </Head>
           <body>
