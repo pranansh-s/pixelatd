ce6abcc7f8a458056c31a493b0fe5553ffe7c12c
Pranansh Singh <43909197+pranansh-s@users.noreply.github.com>
2022-06-11 00:33:20 +0530

    more fixing seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,7 +17,8 @@ import Document, {
         <Html>
           <Head>
           <link rel="icon" href="/favicon.ico" />          
-          <meta name="description" content="Poster/Movie guessing game, where you guess movies from their blurred posters"/>
+          <meta name="description" content="Poster/Movie guessing game, where you guess movies from their blurred posters and genres. Based along the wordle games, play it endlessly and increase your knowledge."/>
+          <meta name="keywords" content="movie, poster, poster guessing, wordle, wordle games, wordle like, movie guess, poster guess, games, movie knowledge, movie quiz, poster quiz, quiz, fun"/>
           <meta property='og:title' content='Pixelatd'/>
           <meta name="google-site-verification" content="GL83bwDes4GKEoNh1nM55lYwcAStf1a5dxKYgpvqtbQ" />
           <link href="https://fonts.googleapis.com/css2?family=Francois+One&family=Gidugu&family=Hind+Madurai:wght@600&display=swap" rel="stylesheet" />
